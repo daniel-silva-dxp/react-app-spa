081a9d88823c6d1a5fe22f09a6d3d7ad7871a81e
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-03 17:42:23 -0300

    feat: restando as configurações do webpack + react

---
+++ src/App/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="my-style">
+                Daniel Silva!
+            </div>
+        );
+    }
+}
+
+export default App;
