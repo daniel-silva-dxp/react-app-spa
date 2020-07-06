4ad35ca0371afdf01109f257a5b2108343354cea
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-06 18:44:02 -0300

    feat: importando a estrutura do app github-app

--- src/App/app.js
+++ src/App/app.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import Home from '../pages/Home'
+
 
 import './style.css';
-import Button from 'components/button';
 
 class App extends Component {
 	render() {
-		return (
-			<div className="my-style">
-				Daniel Silva!
-				<Button>ENTER</Button>
-			</div>
-		);
+		return <Home />
 	}
 }
 
