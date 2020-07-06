bf6fcf45f897e321c839c339473743c4adf93e91
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-06 09:01:25 -0300

    feat: testando os alias

--- src/App/app.js
+++ src/App/app.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
-import './style.css'
+import './style.css';
+import Button from 'components/button';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="my-style">
-                Daniel Silva!
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="my-style">
+				Daniel Silva!
+				<Button>ENTER</Button>
+			</div>
+		);
+	}
 }
 
 export default App;
