8ba47c89632082041053364f0549183bab93efbf
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-06 18:41:57 -0300

    feat: criando a page home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Content from 'components/Content'
+
+class Home extends Component {
+    constructor() {
+		super();
+		this.state = {
+			userInfo: null,
+			repos: [],
+			starred: [],
+			message: 'nenhum usuário do GitHub pesquisado.',
+			isFetching: false
+		};
+	}
+	getGitHubApiUrl(username, type) {
+		const internalUser = username ? `/${username}` : '';
+		const internalType = type ? `/${type}` : '';
+		return `https://api.github.com/users${internalUser}${internalType}`;
+	}
+	handleSearch(e) {
+		const value = e.target.value;
+		const keyCode = e.which || e.keyCode;
+		const ENTER = 13;
+
+		if (keyCode === ENTER) {
+			if (value) {
+				this.setState({ isFetching: true });
+				fetch(this.getGitHubApiUrl(value))
+					.then((data) => data.json())
+					.then((data) => {
+						this.setState({
+							userInfo: {
+								username: data.name,
+								photo: data.avatar_url,
+								login: data.login,
+								repos: data.public_repos,
+								followers: data.followers,
+								following: data.following
+							},
+							repos: [],
+							starred: []
+						});
+					})
+					.finally(() => this.setState({ isFetching: false }));
+			} else {
+				this.setState({
+					userInfo: null,
+					repos: [],
+					starred: [],
+					message: 'insira um nome de usuário do GitHub para pesquisa.'
+				});
+			}
+		}
+	}
+	getRepos(type) {
+		return (e) => {
+			const login = this.state.userInfo.login;
+			fetch(this.getGitHubApiUrl(login, type)).then((data) => data.json()).then((repo) => {
+				this.setState({
+					[type]: repo.map((data) => {
+						return {
+							name: data.name,
+							id: data.id,
+							link: data.html_url
+						};
+					})
+				});
+			});
+		};
+	}
+	render() {
+		return (
+			<Content 
+				userInfo={this.state.userInfo}
+				repos={this.state.repos}
+				starred={this.state.starred}
+				isFetching={this.state.isFetching}
+				handleSearch={(e) => this.handleSearch(e)}
+				getRepos={this.getRepos('repos')}
+				getStarreds={this.getRepos('starred')}
+				message={this.state.message}
+			/>
+		);
+	}
+}
+
+export default Home;
