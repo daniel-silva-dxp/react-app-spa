dcc08f298d1d8c738f3197e2ed4b13b107c31e22
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-06 09:00:29 -0300

    feat: criando alias no webpack para src e components

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
+	devtool: 'source-map',
+	entry: [
 		'react-hot-loader/patch',
 		// 'webpack-dev-server/client?http://localhost:3000',
 		'webpack/hot/only-dev-server',
@@ -16,36 +16,42 @@ module.exports = {
 		filename: '[name]-[hash].js',
 		publicPath: ''
 	},
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                include: /src/,
-                use: { loader: 'babel-loader' }
-            },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                include: /src/,
-                use: { loader: "html-loader" }
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				include: /src/,
+				use: { loader: 'babel-loader' }
+			},
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				include: /src/,
+				use: { loader: 'html-loader' }
+			},
+			{
+				test: /\.css$/i,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
 
-        new HtmlWebpackPlugin({
-            "title": 'SPA with Reactjs',
-            "template": path.join(__dirname, 'public', 'index.html'),
-            "filename": "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]-[hash].css',
-        })
-    ]
+		new HtmlWebpackPlugin({
+			title: 'SPA with Reactjs',
+			template: path.join(__dirname, 'public', 'index.html'),
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name]-[hash].css'
+		})
+	],
+	resolve: {
+		alias: {
+			src: path.join(__dirname, 'src'),
+			components: path.join(__dirname, 'src', 'components')
+		}
+	}
 };
