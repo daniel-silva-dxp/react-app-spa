1bd72b89b82dcd8d700fb0abebd9377a53d8269e
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-07 19:13:04 -0300

    feat: instalando addons do storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
 	stories: [ '../src/**/*.stories.js', '../src/**/*.stories.[tj]s' ],
+	addons: [
+		'@storybook/addon-actions/register',
+		'@storybook/addon-knobs/register',
+		'@storybook/addon-notes/register',
+	],
 	webpackFinal: async (config) => {
 		config.module.rules.push({
 			test: /\.(ts|tsx)$/,
--- stories/Actions.stories.js
+++ stories/Actions.stories.js
@@ -4,10 +4,5 @@ import { storiesOf } from '@storybook/react';
 
 import Actions from '../src/components/Actions'
 
-export default {
-    title: 'Action',
-    component: Actions,
-  };
-
 storiesOf('Button', module)
   .add('Button Default', () => <Actions getRepos={action('GET REPOS')} getStarreds={action('GET STARREDS')}/>)
